f5293ec2763283898df40fa7925de97af26f7676
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-01-27 23:31:50 +0100

    Weitere Umsetzung des revidierten Entscheidungsprozesses (Voranstellen der RPI-Bestimmung!)

--- js/decision.js
+++ js/decision.js
@@ -10,32 +10,36 @@
  */
 var decision = (function() {
 
+    /****************************************************************************************************************
+        Dieses ValueSet wird dynamisch während des Programmablaufes ergänzt bzw. erfährt ein Update über 'setItem'.
+        Hierin stehen dann die bewerteten Kriterien für den Entscheidungsprozess
+    *****************************************************************************************************************/
     var valueSet = {
-        /* Dieses ValueSet wird dynamisch während des Programmablaufes ergänzt bzw. erfährt ein Update über setItem.
-            Hierin stehen dann die validierten und bewerteten Kriterien für den Entscheidungsprozess*/
         
-        /* Hier stehen die Ergebnisse aus dem Entscheidungsprozess,
-            diese werden durch die Funktion executeDecision() befüllt */
-        diagnoses:      ["Diagnoses"],
-        recommends:     ["ToDo's"],
-        errors:         ["Warnings"],
-        maths:          ["Arithmetical"],
-    
-        /* Setzt ein Kriterium für den Entscheidungsprozess fest */
+        /*****************************************************************************************************************
+            Nimmt ein Kriterium für den Entscheidungsprozess in 'valueSet' auf
+            Diese Funktion wird augerufen, wenn die Klinische Patientenkarte und die Laborkarte zusammengestellt werden,
+            sowie wenn der User 'Spieldaten' eingibt.
+        ******************************************************************************************************************/
         setItem:        function(item, val, assessment) {                                                      
                             Object.defineProperty(valueSet, item, { value: { value: val, assess: assessment } } );                            
                         },
         
-        /* Ab hier Definition von Funktionen für den gesamten Entscheidungsprozess */
+        /****************************************************************************** 
+            Ab hier Definition von Funktionen für den gesamten Entscheidungsprozess 
+        *******************************************************************************/
 
         /* Allgemeine, abstrahierte Funktionen für die Tests */
-        hasValue:       function(val) {                                                        
-                            if (valueSet[val] != null) {
+        // Ist das benötigte Entscheidungskriterium überhaupt in 'valueSet' aufgenommen?
+        hasValue:       function(val) {      
+                            // Nur wenn das Entscheidungskriterium vorhanden UND valide ist, dann ...                        
+                            if (valueSet[val] != null && valueSet[val]['assess'].substring(0,2) != 'nv') {
                                 return true;
                             } else {                                
                                 return false;
                             }
                         },
+        // Wie ist die Bewertung des aufgenommen Entscheidungskriterium ausgefallen?
         isValue:        function(val, ass) {                            
                             if (valueSet[val]['assess'] === ass) {
                                 return true;
@@ -54,52 +58,67 @@ var decision = (function() {
         isFerritineLow: function() { return valueSet.isValue('ferritine', 'low'); },        
         hasCRP:         function() { return valueSet.hasValue('crp'); },
         isCrpOK:        function() { return valueSet.isValue('crp', 'ok'); },
-        hasSTFR:        function() { return valueSet.hasValue('sFTR'); },
-        isSFTRNormal:   function() { return valueSet.isValue('sFTR', 'ok'); },
+        hasSTFR:        function() { return valueSet.hasValue('sTFR'); },
+        isSFTRNormal:   function() { return valueSet.isValue('sTFR', 'ok'); },
         hasHematokrit:  function() { return valueSet.hasValue('hematokrit'); },
-        isRPIHigh:      function(limit = 2) {
-                            if (valueSet.rpi() > limit) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        },
         hasVB12:        function() { return valueSet.hasValue('vb12'); },
         isVB12Low:      function() { return valueSet.isValue('vb12', 'low'); },
         hasFolicAcid:   function() { return valueSet.hasValue('folicAcid'); },
         isFolicAcidLow: function() { return valueSet.isValue('folicAcid', 'low'); },
+        hasWeight:      function() { return valueSet.hasValue('weight'); },
 
         /* Berechnungsfunktionen für Werte, die sich aus den gegeben Labordaten ergeben */
-        ironNeeds:          null,
-        rpi:                function() {
-                                return 3; // TESTWEISE!!
+        // Eisenbedarf...
+        ironNeeds:      function() {
+                            if (valueSet.hasWeight()) {
+                            } else {
+                                return null;
+                            }
+                        },
+        // Retikulozyten-Produktions-Index mit Bewertungsfunktion
+        rpi:            function() {
+                            return 1; // TESTWEISE!!
+                        },
+        isRPIHigh:      function(limit = 2) {
+                            if (valueSet.rpi() > limit) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
                             },
+    
+        // Transferrin-Rezeptor-Index
         tfrRIndex:          function() {
 
                             },
 
-        /* Die eigentliche Entscheidungsfunktion */
+        /*********************************
+           Die Entscheidungsfunktion(en)
+        /*********************************/
+        
+        /* Unterentscheidungsalgorithmus 'executeMicrocyticDecisionBranch': In diesen Arm mit multiplen Entscheidungen münden 
+            zwei(!) übergeordnete Entscheidungswege */
         executeMicrocyticDecisionBranch:
-            function() {
+            function() {                
                 if (valueSet.hasFerritine()) {
                     if (valueSet.isFerritineLow()) {
                         // D: Eisenmangelanämie
                         // E: Eisensubstitution                        
                     } else {
-                        if (valueSet.hasCRP()) {
+                        if (valueSet.hasCRP()) {                            
                             if (valueSet.isCrpOK()) {
                                 if (valueSet.isMicrocytic()) {
                                     // D: V.a. Thalassämie
                                     // E: Diagnostik
                                 } else {
                                     // D: Eisenmangel unwahrscheinlich
-                                    // E: Diagnostik
+                                    // E: Diagnostik                                    
                                 }
-                            } else {
-                                if (valueSet.hasSTFR()) {
+                            } else {                                
+                                if (valueSet.hasSTFR()) {                                    
                                     if (valueSet.isSFTRNormal()) {
                                         // D: AOD
-                                        // E: Spezifische Diagnostik
+                                        // E: Spezifische Diagnostik                                        
                                     } else {
                                         // D: Eisenmangel + AOD
                                         // E: 
@@ -119,6 +138,8 @@ var decision = (function() {
                     // E: Ferritin bestimmen
                 }
             },
+        
+        /* Hauptentscheidungsalgorithmus 'executeDecision' */
         executeDecision:    
             function() {                
                 if (valueSet.hasHB()) {
@@ -126,6 +147,49 @@ var decision = (function() {
                         if (valueSet.hasMCV()) {
                             if (valueSet.isMicrocytic()) {
                                 valueSet.executeMicrocyticDecisionBranch();
+                            } else {
+                                if (valueSet.hasHematokrit()) {
+                                    if (valueSet.isRPIHigh(valueSet.rpi())) {
+                                        valueSet.executeMicrocyticDecisionBranch();
+                                    } else {
+                                        if (valueSet.isNormocytic()) {
+                                            // D: V.a. Hypoplastische/Infiltrative/Dyserythropoietische KM-Störung
+                                            // E: Knochenmarkszytologie    
+                                        } else {
+                                            if (valueSet.hasVB12()) {
+                                                if (valueSet.isVB12Low()) {
+                                                    // D: Vit B12-Mangel
+                                                    // E: Substitution
+                                                }
+                                            } else {
+                                                // D:
+                                                // E: Vitamin B12 bestimmen
+                                                valueSet.diagnoses.push("Vit B12 bestimmen");
+                                            }
+                                            if (valueSet.hasFolicAcid()) {
+                                                if (valueSet.isFolicAcidLow()) {
+                                                    // D: Folsäure-Mangel
+                                                    // E: Subistution
+                                                } else {
+                                                    // D: V.a. Hypoplastische/Infiltrative/Dyserythropoietische KM-Störung
+                                                    // E: Knochenmarkszytologie                                                        
+                                                }
+                                            } else {
+                                                // D:
+                                                // E: Folsäure bestimmen
+                                                valueSet.diagnoses.push("Folsäure bestimmen");
+                                            }
+                                        }
+
+                                    }
+                                } else {
+                                    // D: Nicht-Mikrozytäre Anämie
+                                    // E: Hämatokrit bestimmen
+                                }
+                            }
+
+                            /********************************************************************************************
+                            /* Ursprünglicher Algorithmus, in dem normozytär und makrozytär zuerst unterschieden wurden
                             } else if (valueSet.isNormocytic()) {
                                 if (valueSet.hasHematokrit()) {
                                     if (valueSet.isRPIHigh()) {
@@ -176,12 +240,12 @@ var decision = (function() {
 
                                     }
                                 } else {
-                                    // D:
+                                    // D: Makrozytäre Anämie
                                     // E: Hämatokrit bestimmen
-                                }
-                                // D: Makrozytäre Anämie
-                                // E:                         
+                                }                                
                             }
+                            *********************************************************************/
+
                         } else {
                             // D: Anämie 
                             // E: MCV bestimmen
@@ -196,14 +260,26 @@ var decision = (function() {
                 }
             },
 
-        /* Gibt die Ergebnisstruktur zurück */
+        /*******************************************************************
+            Hier stehen die Ergebnisse aus dem Entscheidungsprozess,
+            diese werden durch die Funktion executeDecision() befüllt 
+        ********************************************************************/
+        diagnoses:      ["Diagnoses"],
+        recommends:     ["ToDo's"],
+        errors:         ["Warnings"],
+        maths:          ["Arithmetical"],
+    
+        /* Führt den Entscheidungsprozess durch und gibt die Ergebnisstruktur zurück */        
         result:         function() {
                             valueSet.executeDecision();
-                            return valueSet.diagnoses;
+                            return   valueSet.diagnoses;
                         }
     };
 
-    /* Die Kriterien sind gekapselt, nur die Methoden sind zugänglich */
+    /********************************************************************
+        Die Kriterien und der größte Teil der Methodensind gekapselt, 
+        nur die zwei folgenden Methoden sind zugänglich 
+    *********************************************************************/
     return {
         setItem:    valueSet.setItem,
         result:     valueSet.result        
