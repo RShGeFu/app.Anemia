f69a28498f7a794956458df19311bf163d9d7ee5
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-02-05 08:55:04 +0100

    Einführung einer eigenen Konfigurationsdatei

---
+++ js/config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright bei G. Füchsl - 2018
+ */
+
+"use strict";
+
+var configuration = {
+
+    labTestKit:                         "Roche",     // Dade-Behring-Test möglich
+    labTestKit_tfrFIndexCRP_OK:         3.2,         // Grenzwert für Roche-Test - Grenzwert für Dade-Behring-Test 1.5 (Dtsch Arztebl 2005; 102(9))
+    labTestKit_trfFIndexCRP_High:       2.0,         // Grenzwert für Roche-Test - Grenzwert für Dade-Behring-Test 0.8 (Dtsch Arztebl 2005; 102(9))
+    
+    limitRPI:                           2            // Grenzwert für den Retikulozytenproduktionsindex
+
+}
