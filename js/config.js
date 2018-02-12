eda9fbc8921e9d28d0c8b990c344cb7e7f0cb224
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-02-12 10:00:48 +0100

    Konfigurationsanpassung zwecks Thomasplot

--- js/config.js
+++ js/config.js
@@ -6,9 +6,11 @@
 
 var configuration = {
 
-    labTestKit:                         "Roche",     // Dade-Behring-Test möglich
+    labTestKit:                         "Roche",     // 'Dade-Behring'-Test möglich
     labTestKit_tfrFIndexCRP_OK:         3.2,         // Grenzwert für Roche-Test - Grenzwert für Dade-Behring-Test 1.5 (Dtsch Arztebl 2005; 102(9))
     labTestKit_trfFIndexCRP_High:       2.0,         // Grenzwert für Roche-Test - Grenzwert für Dade-Behring-Test 0.8 (Dtsch Arztebl 2005; 102(9))
+
+    averageRetiHb:                      28,          // in pg, benötigt für den Thomas-Plot
     
     limitRPI:                           2            // Grenzwert für den Retikulozytenproduktionsindex
 
