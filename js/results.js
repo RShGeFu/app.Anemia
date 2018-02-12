77a1becdd98a1b7d03fad6f5b7c99733815c2ab8
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-02-12 10:02:20 +0100

    Anpassung des Thomasplots an die vorgegebenen Werte, incl. Anpassung an CRP-Status

--- js/results.js
+++ js/results.js
@@ -43,7 +43,7 @@ function createCalcValuesCard(mathRes) {
 
 function createIronPlotCard(res, width) {    
     var s = "<div id=\"ironplot-card-body\" class=\"card-body\"><h4 class=\"card-title\"><span id=\"thplot\"></span></h4><table class=\"table table-hover\"><tbody>";
-    s = s + "<canvas id=\"graphics\" width=\"" + width + " \" height=\"300\" style=\"border:1px solid #000000;\">Keine Unterstützung für canvas</canvas>"
+    s = s + "<canvas id=\"graphics\" width=\"" + width + " \" height=\"300\" style=\"border:1px solid #00FF00;\">Keine Unterstützung für Canvas</canvas>"
     return s;
 }
 
@@ -68,43 +68,68 @@ function composeResultCards() {
     htmlString = createIronPlotCard(resData.maths, $("#ironplot-card-body").width());
     $("#ironplot-card").html(htmlString);
 
-    // Test-Plot
-    var ctx = document.getElementById("graphics").getContext('2d');
-    var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+    // Thomas-Plot durchführen
+    // Noch CRP-Normal oder Nicht einbauen!!  
+    var ctx = document.getElementById('graphics').getContext('2d');
+    var scatterChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: 'Margins - x: TfR-F-Index y: Reti-Hb',                
+                data: [ {
+                            x: 0,
+                            y: 0,                            
+                        }, {
+                            x: 0,
+                            y: configuration.averageRetiHb
+                        }, {
+                            x: 0,
+                            y: configuration.averageRetiHb * 2
+                        }, {
+                            x: resData.maths[4] * 2,
+                            y: 0
+                        }, {
+                            x: resData.maths[4] * 2,
+                            y: configuration.averageRetiHb
+                        }, {
+                            x: resData.maths[4] * 2,
+                            y: configuration.averageRetiHb * 2
+                        }, {
+                            x: resData.maths[4],
+                            y: 0
+                        }, {
+                            x: resData.maths[4],
+                            y: configuration.averageRetiHb
+                        }, {
+                            x: resData.maths[4],
+                            y: configuration.averageRetiHb * 2
+                        }],                        
+            },
+            {
+                label: 'Patient',
+                data: [ {
+                            x: resData.maths[2],
+                            y: resData.maths[3]
+                        }],
+                backgroundColor:        '#FF0000',
+                pointRadius:            7,
+                pointHoverRadius:       10,
+                pointHoverBorderColor:  '#0000FF',
+                pointHitRadius:         3
             }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }]
+            },
+            legend: {
+                display: true,
+                position: 'top'
+            }            
         }
-    }
     });
 
     /* Beschriftung nach eingestellter Sprache */
