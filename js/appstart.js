151d2db319515304e8e5a71b23b434aa442b9181
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-02-16 10:52:46 +0100

    Zusammenfassung der Funktionen beim Start der App, nach Laden der index.html

---
+++ js/appstart.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright bei G. Füchsl - 2018
+ */
+
+"use strict";
+
+/**
+ * Nach Laden der App:
+ * 1. Patientenkontext erstellen
+ * 2. Den Reload-Button funktional machen
+ * 3. Die Visualisierung des UI durchführen
+ */
+$(document).ready(function() {
+    
+    /* Patientenkontext herstellen */
+    var pC = getPatientContext(location.search);
+    
+    /* Wenn ein Patientenkontext hergestellt werden konnte */
+    if (typeof pC == 'function') {
+        
+        /* Patientenkontext ausführen */
+        pC();
+
+        /* Patienten- und Ergebniskarten an den Reload-Button hängen */    
+        $("#lab7").click(function () {        
+                pC();
+            });
+
+        /* Revisualisierung des Fensterinhalts bei Resizing des Fensters, insbesondere der Grafikdarstellung */        
+        $(window).resize(function() {
+                composeResultCards();
+            });
+                    
+    } else {
+        /* Das UI zusammenstellen mit Testdaten zusammenstellen
+        composeCards();
+        composeResultCards();    */
+    }
+    
+});
