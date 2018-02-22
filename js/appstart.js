88a3fd0bb578bdf871524978591125f47127c516
gerhard.fuechsl <gerhard.fuechsl@t-online.de>
2018-02-22 23:26:50 +0100

    Reaktion auf User-Eingaben implementiert

--- js/appstart.js
+++ js/appstart.js
@@ -12,18 +12,45 @@
  */
 $(document).ready(function() {
     
-     /* Patientenkontext herstellen, je nach URL-Parameter */
-    var pC = getPatientContext(location.search);
+    /* Patientenkontext herstellen, je nach URL-Parameter */    
+    var pC = getPatientContext(location.search);    
     
     /* Wenn ein Patientenkontext hergestellt werden konnte */
     if (typeof pC == 'function') {
         
         /* Patientenkontext ausführen */
         alert(pC());        
-        /* Patienten- und Ergebniskarten an den Reload-Button hängen */    
-        $("#lab7").click(function () {        
-                pC();
+
+        /* Reaktion auf Werte-Änderungen durch den User an die Input-Elemente einhängen */
+        $(".ds_values").change(function() {
+            
+            /* Referenzbereich abgreifen */        
+            var tnr = testNormalAndValidRange(  this.value, 
+                                                $("#" + this.id + "_refMin").html(), 
+                                                $("#" + this.id + "_refMax").html(), 
+                                                0, 
+                                                $("#" + this.id + "_refMax").html() * 10 );            
+            /* Entscheidungskriterium setzen */
+            decision.setItem(this.id, this.value, tnr.status);            
+            
+            /* Kartenfarbe auf 'verändert' setzen */
+            $(".results").css({
+                                "background-color": "#ffbfbf",
+                                "transition": "0.5s all ease-in-out"
             });
+            
+            /* Neu entscheiden */
+            composeResultCards();            
+        });
+        
+        /* Patienten- und Ergebniskarten an den Reload-Button hängen */    
+        $("#lab7").click(function () {                
+            $(".results").css( {
+                "background-color": "rgb(200, 240, 210)",
+                "transition": "0.5s all ease-in-out"
+            });                
+            window.location.reload();
+        });
 
         /* Revisualisierung des Fensterinhalts bei Resizing des Fensters, insbesondere der Grafikdarstellung */        
         $(window).resize(function() {
